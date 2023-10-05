b7085b50ad57257ab41b88daa826ab47b731d5cd
Gabriele Abrate <gabriele.abrate.2005@gmail.com>
2023-10-05 13:16:01 +0200

    versione 0.0.2 - stessi problemi che possono persistere dalla versione precedente ma adesso ha un temporizzazione che mostra meglio il procedimento del programma e miglioramento della grafica

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+// libreria per la risoluzione di equazioni
 const nerdamer = require('nerdamer/all');
+// vettore di equazioni per poter provare il funzionamento del programma
+const eq = ['x^3-x+1', '4x^2-x+6'];
+// timer utilizzato per il setInterval
+let timer;
 
 /**
  * funzione di bisezione che calcola la soluzione di un'equazione in un intervallo
@@ -12,34 +17,34 @@ function bisezione(intervallo, tolleranza, equazione){
     const x_negativo = nerdamer('-x');
 
     // intestazione
-    console.warn('\n\nfunzione di bisezione');
-    console.log('intervallo: ' + intervallo);
+    console.log('---------------------------');
+    console.log('intervallo: [' + intervallo + ']');
     console.log('tolleranza: ' + tolleranza);
-    console.log('equazione: ' + equazione + '\n\n');
+    console.log('equazione: ' + equazione + '\n');
 
     // calcolo il punto medio dell'intervallo
     let m = (intervallo[0] + intervallo[1]) / 2;
-    console.log('punto medio: ' + m + '\n');
+    console.log('punto medio: ' + m);
 
     // sostituisco x con il punto medio nell'equazione
     let equazioneConSostituzione = equazione.replace(x.toString(), '(' + m + ')');
     // sostituisco -x con il punto medio nell'equazione
     equazioneConSostituzione = equazioneConSostituzione.replace(x_negativo.toString(), '+(' + m + ')');
-    console.log('equazione con sostituzione: ' + equazioneConSostituzione + '\n');
+    console.log('equazione con sostituzione: ' + equazioneConSostituzione);
 
     // Calcola il valore dell'equazione nel punto medio
     const valoreEquazione = nerdamer(equazioneConSostituzione).evaluate();
     console.log('valore dell\'equazione: ' + valoreEquazione.toString() + '\n');
 
     // Se il valore dell'equazione è minore di zero
-    if (valoreEquazione < 0){
+    if (parseInt(valoreEquazione.toString()) < 0){
         // Il punto medio diventa il nuovo estremo inferiore
         console.log('valore dell\'equazione minore di zero');
         intervallo[0] = m;
         console.log('nuovo intervallo: ' + intervallo + '\n');
     }
     // Se il valore dell'equazione è maggiore di zero
-    else if (valoreEquazione > 0){
+    else if (parseInt(valoreEquazione.toString()) > 0){
         // Il punto medio diventa il nuovo estremo superiore
         console.log('valore dell\'equazione maggiore di zero\n');
         intervallo[1] = m;
@@ -48,15 +53,14 @@ function bisezione(intervallo, tolleranza, equazione){
 
     // Se la differenza tra gli estremi è minore della tolleranza
     console.log('if => ' + Math.abs(valoreEquazione) + ' < ' + tolleranza + '\n');
+    console.log('---------------------------');
     // Se la differenza tra gli estremi è minore della tolleranza
     if (Math.abs(valoreEquazione) < tolleranza){
         // Ho trovato una soluzione accettabile
         console.log('Soluzione: ' + m);
+        clearInterval(timer);
         return;
     }
-
-    // Altrimenti, richiamo la funzione
-    bisezione(intervallo, tolleranza, equazione);
 }
 
-bisezione([-2, 0], 0.3, 'x^3-x+1');
+timer = setInterval(bisezione, 1000, [-2, 0], 0.3, eq[0]);
